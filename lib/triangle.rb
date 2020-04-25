b12b38600b030faeb3d5933653ffc00a484cbfda
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-25 22:11:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :base, :height1, :height2
+  
+  def initialize(length1,length2,length3)
+    @base = length1
+    @height1 = length2
+    @height2 = length3 
+  end 
+  
+  def kind 
+    if ((self.base+self.height1) <= self.height2) || ((self.height1+self.height2) <= self.base) || ((self.base+self.height2) <= self.height1)
+              
+              raise TriangleError
+      elsif
+          (self.base == self.height1)  && (self.height1 == self.height2)
+          :equilateral
+    
+     elsif 
+          (self.base == self.height1) || (self.height1 == self.height2) || (self.base == self.height2)
+          :isosceles
+      elsif
+        ((self.base < self.height1) && (self.height1 < self.height2)) || ((self.base > self.height2) && (self.height1 > self.height2))
+        :scalene
+      end 
+  end 
+  
+  class TriangleError < StandardError 
+    def message 
+      puts "Lenghts are not valid"
+    end 
+  end 
+end 
